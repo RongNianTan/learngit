a073adbe5444085a35c9b2fed8d9fba5466a4706
tan <2693513290@qq.com>
2020-10-28 12:11:30 +0800

    vs 测试

---
+++ vs_git_test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace vs_git_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
